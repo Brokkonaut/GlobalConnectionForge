505f9fb531af9633146755b679a5e4cab026899b
Brokkonaut <hannos17@gmx.de>
2023-09-16 08:39:21 +0200

    config cleanup

--- src/main/java/de/cubeside/connection/GlobalClientConfig.java
+++ src/main/java/de/cubeside/connection/GlobalClientConfig.java
@@ -4,23 +4,21 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class GlobalClientConfig {
-    public static ForgeConfigSpec.ConfigValue<String> hostname;
-    public static ConfigValue<Integer> port;
-    public static ForgeConfigSpec.ConfigValue<String> user;
-    public static ForgeConfigSpec.ConfigValue<String> password;
+    public static final ForgeConfigSpec.ConfigValue<String> hostname;
+    public static final ConfigValue<Integer> port;
+    public static final ForgeConfigSpec.ConfigValue<String> user;
+    public static final ForgeConfigSpec.ConfigValue<String> password;
 
     public static final ForgeConfigSpec GENERAL_SPEC;
 
     static {
-        ForgeConfigSpec.Builder configBuilder = new ForgeConfigSpec.Builder();
-        setupConfig(configBuilder);
-        GENERAL_SPEC = configBuilder.build();
-    }
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
-    private static void setupConfig(ForgeConfigSpec.Builder builder) {
         hostname = builder.define("hostname", "localhost");
         port = builder.define("port", 25701);
         user = builder.define("user", "CHANGEME");
         password = builder.define("password", "CHANGEME");
+
+        GENERAL_SPEC = builder.build();
     }
 }
